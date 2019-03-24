c386559a2290b623c72cb444a2fe8e09e51149cb
DmitryAkulich33 <48801723+DmitryAkulich33@users.noreply.github.com>
2019-03-24 15:39:43 +0200

    lesson2
    Scanner

---
+++ task3/src/ArrayTask.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ArrayTask {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);          // объявляем сканер
+        System.out.println("Введите размер массива");      // появляется строка 1
+        int size = scanner.nextInt();                      // вводим размер массива
+        int[] array = new int[size];                       // создаем массив с введенным размером
+        System.out.println("Введите элементы массива");    // появляется строка 2
+        for(int i = 0; i < size; i++){
+            array[i] = scanner.nextInt();                  // заполняем массив
+        }
+        int sum = 0;                                       // вводим переменную sum
+        for(int index = 0; index < array.length; index++){ // считаем сумму элементов массива
+            sum = sum + array[index];
+        }
+        int average = 0;                                   // вводим переменную average
+        average = sum / size;                              // считаем среднее арифметическое массива
+        System.out.println("sum = " + sum);                // выводим на экран сумму
+        System.out.println("average = " + average);        // выводим на экран среднюю величину
+    }
+}
