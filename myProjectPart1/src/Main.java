418fd0764e38ab234ba02183271b7550bcabd6ed
DmitryAkulich33 <Dmitry.Akulich.33@gmail.com>
2019-06-15 17:29:21 +0300

    myProjectPart1

--- myProjectPart1/src/Main.java
+++ myProjectPart1/src/Main.java
@@ -6,8 +6,9 @@ import java.io.IOException;
 
 public class Main {
     public static Robot robot = null;
+    public static BufferedImage image = null;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
             robot = new Robot();
         } catch (AWTException e) {
@@ -15,41 +16,31 @@ public class Main {
         }
 
         for (int i = 0; i < 5; i++) {
+            image = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+            doScreenshot(image);
             System.out.println("Сделан скриншот " + (i + 1));
-            doScreenshot();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            doImageToGrey(image);
+            robot.delay(1000);
         }
     }
 
-    public static void doScreenshot() {
+    public static void doScreenshot(BufferedImage img) throws IOException {
+        ImageIO.write(img, "png", new File("screenshot.png"));
 
-        BufferedImage image = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
-        try {
-            ImageIO.write(image, "png", new File("screenshot.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        double width = image.getWidth();
-        double height = image.getHeight();
+    }
 
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                int rgb = image.getRGB(i, j);
+    public static void doImageToGrey(BufferedImage img) throws IOException {
+
+        for (int i = 0; i < img.getWidth(); i++) {
+            for (int j = 0; j < img.getHeight(); j++) {
+                int rgb = img.getRGB(i, j);
                 int r = (rgb >> 16) & 0xff;
                 int g = (rgb >> 8) & 0xff;
                 int b = rgb & 0xff;
                 int k = (int) (.2126 * r + .7152 * g + .0722 * b);
-                image.setRGB(i, j, (k << 16) | (k << 8) | k);
+                img.setRGB(i, j, (k << 16) | (k << 8) | k);
             }
         }
-        try {
-            ImageIO.write(image, "png", new File("screenshot1.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        ImageIO.write(img, "png", new File("screenshot1.png"));
     }
 }
