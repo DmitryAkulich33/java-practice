02970f5881b32d8f4516a2caeb0d036a4b7f625a
DmitryAkulich33 <Dmitry.Akulich.33@gmail.com>
2019-06-30 16:12:51 +0300

    task12 #2

--- task12/src/Main.java
+++ task12/src/Main.java
@@ -1,94 +1,103 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        int length = 10000000;
+        int length = 1000000;
         int[] arr = new int[length];
-        for(int i = 0; i < length; i++){
-            arr[i] = (int)(Math.random() * 50);
+        for (int i = 0; i < length; i++) {
+            arr[i] = (int) (Math.random() * 50);
         }
-
-        int countsOfThreads1 = 1;
-        int countsOfThreads2 = 2;
-        int countsOfThreads3 = 8;
-
+        int countsOfThreads = 8;
         long startTime1 = System.currentTimeMillis();
-        parallelSortArray(arr, countsOfThreads1);
+        parallelMergeSort(arr, 0, length - 1, countsOfThreads);
         long endTime1 = System.currentTimeMillis();
         System.out.println(endTime1 - startTime1);
-
-        long startTime2 = System.currentTimeMillis();
-        parallelSortArray(arr, countsOfThreads2);
-        long endTime2 = System.currentTimeMillis();
-        System.out.println(endTime2 - startTime2);
-
-        long startTime3 = System.currentTimeMillis();
-        parallelSortArray(arr, countsOfThreads3);
-        long endTime3 = System.currentTimeMillis();
-        System.out.println(endTime3 - startTime3);
     }
 
-    private static void parallelSortArray(int[] array, int count) {
-        if (count <= 1) {
-            sortArray(array);
-        } else if (array.length >= 2) {
-            int[] arrayLeft = Arrays.copyOfRange(array, 0, array.length / 2);
-            int[] arrayRight = Arrays.copyOfRange(array, array.length / 2, array.length);
-
-            Thread leftThread = new Thread() {
-                public void run() {
-                    parallelSortArray(arrayLeft, count / 2);
-                }
-            };
-            Thread rightThread = new Thread() {
-                public void run() {
-                    parallelSortArray(arrayRight, count / 2);
-                }
-            };
-
-            leftThread.start();
-            rightThread.start();
-
-            try {
-                leftThread.join();
-                rightThread.join();
-            } catch (InterruptedException e) {
-
-            }
-            mergeArray(arrayLeft, arrayRight);
-        }
-    }
-
-    private static int[] sortArray(int[] array){
-        if(array.length < 2){
-            return array;
-        }
-        int middle = array.length/2;
-        int[] arrayLeft = Arrays.copyOfRange(array, 0, middle);
-        int[] arrayRight = Arrays.copyOfRange(array, middle, array.length);
-        System.arraycopy(array, array.length/2, arrayRight, 0, array.length - array.length/2);
-        arrayLeft = sortArray(arrayLeft);
-        arrayRight = sortArray(arrayRight);
-        return mergeArray(arrayLeft, arrayRight);
-    }
-
-    private static int[] mergeArray(int[] arrayA, int[] arrayB){
-        int [] arrayC = new int[arrayA.length + arrayB.length];
-        int positionA = 0;
-        int positionB = 0;
-        for (int i = 0; i < arrayC.length; i++) {
-            if (positionA == arrayA.length){
-                arrayC[i] = arrayB[positionB++];
-            } else if (positionB == arrayB.length) {
-                arrayC[i] = arrayA[positionA++];
-            } else if (arrayA[positionA] < arrayB[positionB]) {
-                arrayC[i] = arrayA[positionA++];
+    private static void parallelMergeSort(int[] array, int start, int end, int count){
+        if(end - start > 0){
+            if (count <= 1){
+                mergeSort(array, start, end);
             } else {
-                arrayC[i] = arrayB[positionB++];
+                Thread leftArray = new Thread(){
+                    public void run(){
+                        parallelMergeSort(array, start, end/2, count - 1);
+                    }
+                };
+                Thread rightArray = new Thread(){
+                    public void run(){
+                        parallelMergeSort(array, (end/2 + 1), end, count - 1);
+                    }
+                };
+                leftArray.start();
+                rightArray.start();
+
+                try {
+                    leftArray.join();
+                    rightArray.join();
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+
+                merge(array, start, end/2, end);
             }
         }
-        return arrayC;
+    }
+
+    private static void mergeSort(int[] array, int start, int end) {
+        if (start == end) {
+            return;
+        }
+        if (end - start > 0) {
+            mergeSort(array, start, (start + end) / 2);
+            mergeSort(array, (start + end) / 2 + 1, end);
+            merge(array, start, (start + end) / 2, end);
+        }
+    }
+
+    private static void merge(int[] array, int start, int mid, int end) {
+        int[] newArray = new int[array.length];
+        int begin1 = start;
+        int begin2 = mid + 1;
+        int index = start;
+        while(begin1 <= mid && begin2 <= end)
+        {
+            if(array[begin1]<array[begin2])
+            {
+                newArray[index] = array[begin1];
+                begin1 = begin1 + 1;
+            }
+            else
+            {
+                newArray[index] = array[begin2];
+                begin2 = begin2+1;
+            }
+            index++;
+        }
+        if(begin1 > mid)
+        {
+            while(begin2 <= end)
+            {
+                newArray[index] = array[begin2];
+                index++;
+                begin2++;
+            }
+        }
+        else
+        {
+            while(begin1 <= mid)
+            {
+                newArray[index] = array[begin1];
+                index++;
+                begin1++;
+            }
+        }
+        int k = start;
+        while(k < index)
+        {
+            array[k] = newArray[k];
+            k++;
+        }
     }
 }
+
 
